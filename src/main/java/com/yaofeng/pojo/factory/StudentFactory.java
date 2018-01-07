d04acf6d07adfa38a457db3a6658b8a89c44ac71
yaofeng8888 <461563387@qq.com>
2018-01-07 11:54:15 +0800

    spring入门

---
+++ src/main/java/com/yaofeng/pojo/factory/StudentFactory.java
@@ -0,0 +1,10 @@
+package com.yaofeng.pojo.factory;
+
+import com.yaofeng.pojo.po.Student;
+
+public class StudentFactory {
+	public static Student createFactory() {
+		return new Student();
+	}
+	
+}
---
+++ src/test/java/com/yaofeng/pojo/po/StudentTest.java
@@ -0,0 +1,24 @@
+package com.yaofeng.pojo.po;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentTest {
+
+	@SuppressWarnings("resource")
+	@Test
+	public void test1() {
+		//³õÊ¼»¯IoCÈÝÆ÷
+			ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		/*	Student bean = (Student) context.getBean("student");
+			Student bean2 = context.getBean("studentname", Student.class);
+			System.out.println(bean);
+			System.out.println(bean2);*/
+			
+			Student factory = (Student) context.getBean("studentfactory");
+			System.out.println(factory);
+
+	}
+}
